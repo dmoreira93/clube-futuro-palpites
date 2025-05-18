d5fee52527eb7c0393b3640bdb3a8752a840c1b1
dmoreira93 <150024580+dmoreira93@users.noreply.github.com>
2025-05-18 23:07:32 +0000

    enviar alterações

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,11 +1,7 @@
-// src/utils/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Variáveis de ambiente do Supabase não definidas');
-}
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
