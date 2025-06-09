856f057f6d5b05de971a99cc8b87a23cf784e2c0
dmoreira93 <150024580+dmoreira93@users.noreply.github.com>
2025-06-09 12:11:04 +0000

    teste

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,9 @@
 // src/components/layout/Layout.tsx
 import React from 'react';
-import Navbar from './Navbar';
+// import Navbar from './Navbar'; // A importação pode ser comentada também
 import Footer from './Footer';
 import { Toaster } from "@/components/ui/toaster"
-import { BottomNavbar } from './BottomNavbar'; // <-- Importe o novo componente
+import { BottomNavbar } from './BottomNavbar';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -12,14 +12,14 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
-      {/* Adiciona padding-bottom para o conteúdo não ficar atrás da navbar no mobile */}
+      {/* <Navbar /> */} {/* <-- Navbar PRINCIPAL COMENTADA */}
+
       <main className="flex-grow container mx-auto p-4 pb-20 md:pb-4">
         {children}
       </main>
       <Toaster />
       <Footer />
-      <BottomNavbar /> {/* <-- Adicione o componente aqui */}
+      <BottomNavbar />
     </div>
   );
 };
