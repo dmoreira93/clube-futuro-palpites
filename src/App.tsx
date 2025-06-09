7ead1dc05f504b1c1bfc5c26ee5a6df80c62fd63
dmoreira93 <150024580+dmoreira93@users.noreply.github.com>
2025-06-09 12:38:42 +0000

    Passo 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-// Arquivo App.tsx - Teste do Passo 1
+// Arquivo App.tsx - Teste do Passo 2
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
-    <div style={{ padding: '2rem', fontSize: '2rem', color: 'black', backgroundColor: 'white' }}>
-      Olá, Mundo!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <div style={{ padding: '2rem', fontSize: '2rem', color: 'black', backgroundColor: 'white' }}>
+            Página com Roteador
+          </div>
+        } />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
