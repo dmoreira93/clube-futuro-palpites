16ad8d473c9e3126b57e8cbeb26b46f8efd5ae09
dmoreira93 <150024580+dmoreira93@users.noreply.github.com>
2025-06-09 12:02:48 +0000

    teste comment

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -107,11 +107,11 @@ const Index = () => {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           <div className="lg:col-span-2">
             {/* Se o erro persistir, o próximo passo é comentar a linha abaixo */}
-            <RankingTable />
+            //<RankingTable />
           </div>
           <div className="lg:col-span-1 flex flex-col gap-8">
             {/* E também comentar a linha abaixo */}
-            <NextMatches />
+            //<NextMatches />
             <div className="hidden lg:block">
               <Card className="shadow-lg">
                 <CardHeader>
