c5d884ee12d167728fabb8440899f72b12a15a27
dmoreira93 <150024580+dmoreira93@users.noreply.github.com>
2025-06-10 17:50:20 +0000

    asd

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 // src/main.tsx
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query' // <-- NOVAS IMPORTAÇÕES
+
+// Crie uma instância do QueryClient
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+    {/* Envolva toda a sua aplicação com o QueryClientProvider */}
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
