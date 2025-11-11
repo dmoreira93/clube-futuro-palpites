7592084bb455df0e1df9af6d12b90e887b9ecd52
Diego Moreira <150024580+dmoreira93@users.noreply.github.com>
2025-11-11 19:54:50 +0000

    Revert "Connect to Lovable Cloud"
    This reverts commit dc562edaf33eff02a6f08ec94c09358e3df4628c.

--- supabase/migrations/20251110111755_f5f9d988-a0ed-4120-936d-c2be9c6b6c79.sql
+++
@@ -1,8 +0,0 @@
--- Adiciona coluna api_football_id à tabela teams se não existir
-DO $$ 
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM information_schema.columns 
-                   WHERE table_schema = 'public' AND table_name = 'teams' AND column_name = 'api_football_id') THEN
-        ALTER TABLE public.teams ADD COLUMN api_football_id INTEGER;
-    END IF;
-END $$;
