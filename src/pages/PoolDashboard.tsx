391ee47e2226318ee7b1f3e5e627c7a77db3dd29
Diego Moreira <150024580+dmoreira93@users.noreply.github.com>
2025-11-21 23:20:49 +0000

    att manual

--- src/pages/PoolDashboard.tsx
+++ src/pages/PoolDashboard.tsx
@@ -20,7 +20,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 interface PoolHeaderData {
   name: string;
   invite_code: string;
-  description?: string;
+  //description?: string;
 }
 
 const PoolDashboard = () => {
